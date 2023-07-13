721f854650f117accf70ae5e9b8d4a7faf6d9ba2
joseedivandro <jose.edivandro.ja@outlook.com>
2023-07-12 22:26:10 -0300

    feat(hotels): ideia inicial para rota hotels

---
+++ src/repositories/hotel-repository/index.ts
@@ -0,0 +1,24 @@
+import { prisma } from '@/config';
+
+async function findHotels() {
+  const hotels = prisma.hotel.findMany();
+  return hotels;
+}
+
+async function findRoomsByHotelId(hotelId: number) {
+  return prisma.hotel.findFirst({
+    where: {
+      id: hotelId,
+    },
+    include: {
+      Rooms: true,
+    },
+  });
+}
+
+const hotelRepository = {
+  findHotels,
+  findRoomsByHotelId,
+};
+
+export default hotelRepository;
